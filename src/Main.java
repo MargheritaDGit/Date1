ef6f388bacc5b0c5e30781fd7df1fdb363aec28b
Margherita <margherita.deodato14@gmail.com>
2023-11-29 16:59:35 +0100

    commit date1 sistemato in live con nicola

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,6 @@
 //Stampa le varie versioni
 
 import java.time.OffsetDateTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 
@@ -12,18 +10,14 @@ public class Main {
     public static void main(String[] args) {
 
 
-        String inputString =  "2002-03-01T13:00:00Z"; /*"2002-03-01T13:00:00+00:00Z"; "2002-03-01T13:00:00Z";*/
+        String inputString = "2002-03-01T13:00:00Z";
         OffsetDateTime offsetDateTime = OffsetDateTime.parse(inputString);
-        //ZoneId zoneIdOffset = ZoneId.ofOffset("UTC", ZoneOffset.UTC);
-       // ZoneId zoneIdUtc = ZoneId.of("UTC");
+
 
         String formattedFull = offsetDateTime.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL));
         String formattedMedium = offsetDateTime.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM));
         String formattedShort = offsetDateTime.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT));
 
-       // ZoneId zone;
-        //zone = offsetDateTime.getZone();
-        ZoneId zone = offsetDateTime.getOffset();
 
         System.out.println("La versione FULL è: " + formattedFull);
         System.out.println("La versione MEDIUM è: " + formattedMedium);
